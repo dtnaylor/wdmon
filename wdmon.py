f64389d7a448705e894fc5eeb451ebfc9fb217b1
David Naylor <dnaylor@nefeli.io>
2022-01-02 13:24:40 -0800

    wdmon.py: Add --csv output option

--- wdmon.py
+++ wdmon.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import argparse
 import numpy as np
+import os
 import sys
 
 from collections import namedtuple
@@ -8,7 +10,9 @@ import xml.etree.ElementTree as ET
 
 Sample = namedtuple("Sample", "id timestamp snr rssi rate noise")
 
-def get_samples(root):
+def get_samples(file):
+    tree = ET.parse(file)
+    root = tree.getroot()
     for child in root:
         if "type" in child.attrib and child.attrib["type"] == "WDSIGNALGRAPHSAMPLE":
             yield child
@@ -23,24 +27,46 @@ def parse_sample(sample):
         int(sample.find("./attribute[@name='noise']").text),
     )
 
-def print_summary(samples):
+def print_ascii_summary(name, samples):
+    def summarize_feature(values):
+        print(f"  min/p25/med/p75/max:\t{np.min(values)}/{np.percentile(values, 25)}/{np.median(values)}/{np.percentile(values, 75)}/{np.max(values)}")
+        print(f"                 mean:\t{np.mean(values):.2f}")
+    
+    print("="*10 + f" {name} " + "="*10)
+
     snrs = [s.snr for s in samples]
     print("SNR:")
-    print(f"  min/p25/med/p75/max:\t{np.min(snrs)}/{np.percentile(snrs, 25)}/{np.median(snrs)}/{np.percentile(snrs, 75)}/{np.max(snrs)}")
-    print(f"                 mean:\t{np.mean(snrs):.2f}")
+    summarize_feature(snrs)
     
     rates = [s.rate for s in samples]
     print("Tx Rate:")
-    print(f"  min/p25/med/p75/max:\t{np.min(rates)}/{np.percentile(rates, 25)}/{np.median(rates)}/{np.percentile(rates, 75)}/{np.max(rates)}")
-    print(f"                 mean:\t{np.mean(rates):.2f}")
+    summarize_feature(rates)
 
-def main():
-    tree = ET.parse(sys.argv[1])
-    root = tree.getroot()
 
-    samples = [parse_sample(s) for s in get_samples(root)]
+def print_csv_summary(name, samples):
+    def summarize_feature(values):
+        return f"{np.mean(values)},{np.min(values)},{np.percentile(values, 25)},{np.median(values)},{np.percentile(values, 75)},{np.max(values)}"
+    
+    snrs = [s.snr for s in samples]
+    rates = [s.rate for s in samples]
+    print(f"{name},{summarize_feature(snrs)},{summarize_feature(rates)}")
 
-    print_summary(samples)
+
+def main(args):
+    if args.csv:
+        print("file,mean-snr,min-snr,p25-snr,median-snr,p75-snr,max-snr,mean-rate,min-rate,p25-rate,median-rate,p75-rate,max-rate")
+
+    for file in args.files:
+        samples = [parse_sample(s) for s in get_samples(file)]
+        if args.csv:
+            print_csv_summary(os.path.basename(file), samples)
+        else:
+            print_ascii_summary(os.path.basename(file), samples)
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files", nargs="*", help=".wdmon WiFi performance trace files")
+    parser.add_argument("--csv", action="store_true", help="Print results as CSV")
+
+    main(parser.parse_args())
